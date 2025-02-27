c4e062545df2cd84086a652b1972659c273d682e
AndresQuijano <andres.quijano@iovlabs.org>
2025-02-27 17:08:50 +0000

    fix: fix eslint errors

--- integration-test/test/isQuotePaid.test.ts
+++ integration-test/test/isQuotePaid.test.ts
@@ -1,6 +1,6 @@
 import { describe, test, expect, beforeAll } from '@jest/globals'
-import { assertTruthy, BlockchainConnection, Network } from '@rsksmart/bridges-core-sdk'
-import { AcceptedQuote, PeginQuoteRequest, Quote } from '../../src/api'
+import { assertTruthy, BlockchainConnection } from '@rsksmart/bridges-core-sdk'
+import type { AcceptedQuote, PeginQuoteRequest, Quote } from '../../src/api'
 import { integrationTestConfig } from '../config'
 import { fakeTokenResolver } from './common/utils'
 import { Flyover } from '../../src/sdk/flyover'
@@ -20,59 +20,58 @@ describe('isQuotePaid function should', () => {
 
   beforeAll(async () => {
     flyover = new Flyover({
-      network: integrationTestConfig.network as Network,
+      network: integrationTestConfig.network,
       allowInsecureConnections: true,
       captchaTokenResolver: fakeTokenResolver,
       disableChecksum: true
     })
-    
+
     const rskConnection = await BlockchainConnection.createUsingPassphrase(
-        integrationTestConfig.testMnemonic,
-        integrationTestConfig.nodeUrl
+      integrationTestConfig.testMnemonic,
+      integrationTestConfig.nodeUrl
     )
 
     await flyover.connectToRsk(rskConnection)
   })
 
   test('return true if the quote is paid', async () => {
-    //Get provider
+    // Get provider
     const providers = await flyover.getLiquidityProviders()
 
     const provider = providers.find(p => p.id === integrationTestConfig.providerId)
     assertTruthy(provider, 'Provider not found')
     flyover.useLiquidityProvider(provider)
-    console.log(`Provider selected. Id: ${provider.id}, name: ${provider.name}`);
+    console.log(`Provider selected. Id: ${provider.id}, name: ${provider.name}`)
 
-    //Get quote
-    const request = {
+    // Get quote
+    const request: PeginQuoteRequest = {
       callEoaOrContractAddress: integrationTestConfig.rskAddress,
       callContractArguments: '',
       valueToTransfer: integrationTestConfig.peginAmount,
       rskRefundAddress: integrationTestConfig.rskAddress
-    } as PeginQuoteRequest
+    }
     quotes = await flyover.getQuotes(request)
 
     expect(quotes.length).toBeGreaterThan(0)
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     quote = quotes[0]!
-    console.log(`Quote received. Hash: ${quote.quoteHash}`);
 
-    //Accept quote
+    // Accept quote
     acceptedQuote = await flyover.acceptQuote(quote)
 
     expect(acceptedQuote.signature).not.toBeUndefined()
     expect(acceptedQuote.bitcoinDepositAddressHash).not.toBeUndefined()
 
     console.log(`Please proceed to pay the quote. \n BTC Address:  ${acceptedQuote.bitcoinDepositAddressHash}
-      \n amount: ${quote.quote.value} \nTime for deposit: ${quote.quote.timeForDeposit} seconds`);
+      \n amount: ${quote.quote.value} \nTime for deposit: ${quote.quote.timeForDeposit} seconds`)
 
     // Wait for 1.5 times the deposit time
     const waitTimeMs = quote.quote.timeForDeposit * 1000 * 1.5
     console.log(`Waiting for ${waitTimeMs / 1000} seconds...`)
     await new Promise(resolve => setTimeout(resolve, waitTimeMs))
 
-    //Check if the quote is paid
+    // Check if the quote is paid
     const response = await flyover.isQuotePaid(quote.quoteHash)
     expect(response.isPaid).toBe(true)
   }, 200000)
 })
-
