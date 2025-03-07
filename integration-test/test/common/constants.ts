cee4097154b098fde7f3bd4a6a61b3274a77907f
Luisfc68 <luisfc68@gmail.com>
2025-03-07 19:09:50 -0300

    test: update test contract abi

--- integration-test/test/common/constants.ts
+++ integration-test/test/common/constants.ts
@@ -1,5 +1,8 @@
 export const EXTENDED_TIMEOUT = 7200 * 1000
 export const TEST_CONTRACT_ABI = [
   'function save(address destination) public payable',
-  'function setValue(uint256 _value) external'
+  'function setValue(uint256 _value) external',
+  'function retrieveValue() external view returns (uint256)',
+  'function getBalance(address destination) external view returns (uint256)',
+  'function withdraw() external'
 ]
